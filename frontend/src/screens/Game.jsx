3229db57f1405151fd70e46ce71ac6bd6209ec37
Nathan Johnson <nathancj224@gmail.com>
2022-12-30 18:24:00 -0500

    generate QR code for game

--- frontend/src/screens/Game.jsx
+++ frontend/src/screens/Game.jsx
@@ -1,7 +1,18 @@
-import { Text } from '@chakra-ui/react';
+import { Stack, Heading, Box } from '@chakra-ui/react';
+import QRCode from 'react-qr-code';
 import { useParams } from 'react-router-dom';
+import { LogoHeader } from '../components/LogoHeader';
 
 export function GameScreen() {
   const { gameId } = useParams();
-  return <Text>{gameId}</Text>;
+  const qrCodeUrl = `${window.location.origin}/join/${gameId}`;
+  return (
+    <Stack align="flex-start" alignItems="center">
+      <LogoHeader width={200} />
+      <Heading>{gameId}</Heading>
+      <Box padding={5} bg="white">
+        <QRCode value={qrCodeUrl} />
+      </Box>
+    </Stack>
+  );
 }
