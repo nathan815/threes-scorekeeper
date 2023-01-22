8348a67ab892bde03374aef00ed5b0353720df42
Nathan Johnson <nathancj224@gmail.com>
2023-01-22 13:17:45 -0500

    prepend emotion css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,24 @@
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import theme from './theme';
 import { AuthProvider } from './auth/authContext';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import theme from './theme';
+
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+
+const emotionCache = createCache({
+  key: 'emotion-css-cache',
+  prepend: true, // ensures styles are prepended to the <head>, instead of appended
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <ChakraProvider theme={theme}>
+  <CacheProvider value={emotionCache}>
+    <ChakraProvider theme={theme} value={emotionCache}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <React.StrictMode>
         <AuthProvider>
@@ -18,7 +26,7 @@ root.render(
         </AuthProvider>
       </React.StrictMode>
     </ChakraProvider>
-  </>
+  </CacheProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
