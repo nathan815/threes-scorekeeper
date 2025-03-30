e728353733c30e8a22cc7f12f2509012723d30e3
Nathan Johnson <nathancj224@gmail.com>
2025-03-30 02:24:53 -0400

    changes to attempt to fix the broken css includes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,33 @@
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import { Global } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { CacheProvider, Global } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { prefixer } from 'stylis';
 import App from './App';
 import { AuthProvider } from './auth/authContext';
 import * as styles from './index.css';
 import reportWebVitals from './reportWebVitals';
 import theme from './theme';
 
+const myCache = createCache({
+  key: 'emotion-cache',
+  stylisPlugins: [prefixer],
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider theme={theme}>
-    <Global styles={styles} />
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <React.StrictMode>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </React.StrictMode>
-  </ChakraProvider>
+  <CacheProvider value={myCache}>
+    <ChakraProvider theme={theme}>
+      <Global styles={styles} />
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <React.StrictMode>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </React.StrictMode>
+    </ChakraProvider>
+  </CacheProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
